dcfec0c88934831754cc3fd22ea47eb9b7fe71f5
Brian A <50188700+brianabplanalp1@users.noreply.github.com>
2020-07-07 18:23:43 +0100

    Add files via upload

---
+++ build/precache-manifest.7fcabf769fca0737762d08330d93c759.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5107e5b61a8fd98393fdc92c281cc62",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f5688e95d1e7a6dd6fc2",
+    "url": "/weather-app/static/css/main.1fb04bb0.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5688e95d1e7a6dd6fc2",
+    "url": "/weather-app/static/js/main.e94fb55b.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "efb6ecc278dc6c1c01b7ea94e4091286",
+    "url": "/weather-app/static/media/cold.efb6ecc2.jpg"
+  },
+  {
+    "revision": "7a5722aebd1dcbc3a2c0bec96cf1774d",
+    "url": "/weather-app/static/media/warm.7a5722ae.jpg"
+  }
+]);
